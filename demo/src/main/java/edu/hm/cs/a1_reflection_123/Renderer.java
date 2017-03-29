a9c4e29f376ea15cb6247cfb0fbcdb466329a67e
Matthias Herpers <herpers.matthias0@hm.edu>
2017-03-29 14:35:25 +0200

    more

--- demo/src/main/java/edu/hm/cs/a1_reflection_123/Renderer.java
+++ demo/src/main/java/edu/hm/cs/a1_reflection_123/Renderer.java
@@ -1,5 +1,7 @@
 package edu.hm.cs.a1_reflection_123;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by MatHe on 29.03.2017.
  */
@@ -15,6 +17,30 @@ public class Renderer {
 
 
         Class<?> cut = obj.getClass();
+
+
+        Field[] fields = cut.getFields();
+
+        for (Field field: fields){
+            if (field.getAnnotation(edu.hm.cs.a1_reflection_123.RenderMe.class) != null) {
+                field.
+
+
+                /*
+                tests++;
+				Object ott = cut.getConstructor().newInstance();
+				try {
+					method.invoke(ott);
+					successes++;
+				} catch(InvocationTargetException e){
+					fails++;
+				}
+                 */
+            }
+
+
+
+        }
 
 
         return "";
