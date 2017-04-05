932b0c8ada3cb55ff064c00cd1eaa0bc0a9295ab
Matthias Herpers <herpers.matthias0@hm.edu>
2017-04-05 13:49:43 +0200

    arrayrenderer

--- demo/src/main/java/edu/hm/cs/a1_reflection_123/Renderer.java
+++ demo/src/main/java/edu/hm/cs/a1_reflection_123/Renderer.java
@@ -1,6 +1,10 @@
 package edu.hm.cs.a1_reflection_123;
 
+import edu.hm.renderer.ArrayRenderer;
+
 import java.lang.reflect.Field;
+
+import static java.lang.Class.*;
 
 /**
  * Created by MatHe on 29.03.2017.
@@ -23,6 +27,7 @@ public class Renderer implements IRenderer {
      * @return string to use in JUnit.
      */
     public String render() {
+        ArrayRenderer renderArray = new ArrayRenderer();
         String res = "";
 
         Class< ? > cut = obj.getClass();
@@ -32,26 +37,42 @@ public class Renderer implements IRenderer {
 
         for (Field field: fields) {
             if (field.getAnnotation(edu.hm.cs.a1_reflection_123.RenderMe.class) != null) {
-                RenderMe a = field.getAnnotation(edu.hm.cs.a1_reflection_123.RenderMe.class);
-                if (a.with().equals("edu.hm.cs.a1_reflection_123.Renderer")){
-                    System.out.println("wked");
+                RenderMe annotation = field.getAnnotation(edu.hm.cs.a1_reflection_123.RenderMe.class);
+                    if (annotation.with().equals("edu.hm.cs.a1_reflection_123.Renderer")){
+                        //region normal
+                        try {
+                            field.setAccessible(true);
+                            Object abw = field.get(obj);
+                            res += field.getName() + " ";
+                            if (field.getType().isPrimitive()) {
+                                res +=  "(Type " + field.getType() + "): ";
+                            }
+                            else {
+                                res +=  "(Type " + field.getType().toString().substring(LENGTH_OF_CLASS_STRING) + "): ";
+                            }
+                            res += field.get(obj).toString();
+                            res += "\n";
+                        } catch(IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                        //endregion
+                    }
+                    else{
+                        //region array
+                        Object intArray= new Object();
+                        field.setAccessible(true);
+                        try {
+                             intArray = field.get(obj);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+
+                        res+= renderArray.render((int[])intArray);
+
+                        //endregion
+
                 }
 
-                try {
-                    field.setAccessible(true);
-                    Object abw = field.get(obj);
-                    res += field.getName() + " ";
-                    if (field.getType().isPrimitive()) {
-                        res +=  "(Type " + field.getType() + "): ";
-                    }
-                    else {
-                        res +=  "(Type " + field.getType().toString().substring(LENGTH_OF_CLASS_STRING) + "): ";
-                    }
-                    res += field.get(obj).toString();
-                    res += "\n";
-                } catch(IllegalAccessException e) {
-
-                }
 
             }
 
--- demo/src/main/java/edu/hm/renderer/ArrayRenderer.java
+++ demo/src/main/java/edu/hm/renderer/ArrayRenderer.java
@@ -5,10 +5,11 @@ import edu.hm.cs.a1_reflection_123.IRenderer;
 /**
  * Created by MatHe on 05.04.2017.
  */
-public class ArrayRenderer implements IRenderer {
-    public String render() {
+public class ArrayRenderer {
+    public String render(int[] array) {
         String result = "";
 
-        return null;
+
+        return result;
     }
 }
